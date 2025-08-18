c98db6954ca34bcd97e9c39f59357a9d20351f43
tasnim29 <tasnimmahmudnjr@gmail.com>
2025-08-18 09:55:25 +0600

    finished

--- problem9.js
+++ problem9.js
@@ -2,7 +2,6 @@ function factorial(num) {
   let factorialResult = 1;
   for (let i = 1; i <= num; i++) {
     factorialResult = factorialResult * i;
-    console.log(factorialResult);
   }
   return factorialResult;
 }
